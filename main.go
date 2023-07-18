0632384380bb7a6d1869d1393013c61d7dbf3311
enesconf <enes46ctknya@gmail.com>
2023-07-18 12:32:02 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type ImprovementSummary struct {
+	QuestionId         string `json:"QuestionId"`
+	PillarId           string `json:"PillarId"`
+	QuestionTitle      string `json:"QuestionTitle"`
+	Risk               string `json:"Risk"`
+	ImprovementPlanUrl string `json:"ImprovementPlanUrl"`
+}
+
+type Workload struct {
+	WorkloadId            string               `json:"WorkloadId"`
+	LensAlias             string               `json:"LensAlias"`
+	LensArn               string               `json:"LensArn"`
+	ImprovementSummaries []ImprovementSummary `json:"ImprovementSummaries"`
+}
+
+func main() {
+	jsonFile, err := os.Open("/Users/enescetinkaya/Desktop/projects/wellarc/high_risk_improvements.json")
+	if err != nil {
+		fmt.Println("Error opening JSON file:", err)
+		return
+	}
+	defer jsonFile.Close()
+
+	var workload Workload
+	jsonParser := json.NewDecoder(jsonFile)
+	err = jsonParser.Decode(&workload)
+	if err != nil {
+		fmt.Println("Error decoding JSON:", err)
+		return
+	}
+
+	csvFile, err := os.Create("output.csv")
+	if err != nil {
+		fmt.Println("Error creating CSV file:", err)
+		return
+	}
+	defer csvFile.Close()
+
+	writer := csv.NewWriter(csvFile)
+	defer writer.Flush()
+
+	err = writer.Write([]string{"Card Name", "Card Description", "Labels", "List Name", "Checklist", "Checklist item"})
+	if err != nil {
+		fmt.Println("Error writing CSV header:", err)
+		return
+	}
+
+	for _, summary := range workload.ImprovementSummaries {
+		row := []string{
+			summary.QuestionId,
+			summary.QuestionTitle,
+			summary.Risk,
+			summary.PillarId,
+			summary.ImprovementPlanUrl,
+			"", // Empty as there's no corresponding field in your JSON
+		}
+		err = writer.Write(row)
+		if err != nil {
+			fmt.Println("Error writing CSV row:", err)
+			return
+		}
+	}
+}
+
